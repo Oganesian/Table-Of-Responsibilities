f450f1b65451895dc7baedbfb770d66949564292
Karen <bezimeninofamily@gmail.com>
2018-06-05 12:47:22 +0200

    created the Steward class

---
+++ Table-Of-Responsibilities/Steward.cs
@@ -0,0 +1,45 @@
+﻿namespace Table_Of_Responsibilities
+{
+    class Steward
+    {
+        string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        string surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        bool canBeTheManager
+        {
+            get { return canBeTheManager; }
+            set { canBeTheManager = value; }
+        }
+
+        bool canUseTheControlPanel
+        {
+            get { return canUseTheControlPanel; }
+            set { canUseTheControlPanel = value; }
+        }
+
+        bool canServeWithAMicrophone
+        {
+            get { return canServeWithAMicrophone; }
+            set { canServeWithAMicrophone = value; }
+        }
+
+        void saveToFile()
+        {
+
+        }
+
+        string getFromFile()
+        {
+            return "";
+        }
+    }
+}
